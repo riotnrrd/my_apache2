72165c36a40aad9c32de87af948f0866b1386222
riotnrrd <r10tn3rd@yahoo.com>
2013-10-24 14:15:37 -0400

    initial commit

---
+++ attributes/default.rb
@@ -0,0 +1,3 @@
+default['apache']['sites']['clowns'] = { "port" => 80, "image" => "http://jimovelmen.com/csula/syllabi_use/383_pics/menagerie.jpg" }
+default['apache']['sites']['bears'] = { "port" => 81, "image" => "http://jimovelmen.com/csula/syllabi_use/383_pics/menagerie.jpg" }
+default['apache']['sites']['ponies'] = { "port" => 83, "image" => "http://jimovelmen.com/csula/syllabi_use/383_pics/menagerie.jpg" }
